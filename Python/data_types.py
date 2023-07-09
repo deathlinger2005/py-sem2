7ebf7f37de406dce24adc463bdd6fa94040e09ad
deathlinger2005 <69686944+deathlinger2005@users.noreply.github.com>
2023-07-09 12:48:09 +0000

    basic data types added

---
+++ Python/data_types.py
@@ -0,0 +1,22 @@
+# Basic Python data types
+
+# Integer
+a = 1
+print(a)
+
+# String
+b = "Arpit"
+print(b)
+
+# List
+l = [1,2,"aabc"]
+print(l)
+
+#tuple 
+
+t = (1,2,3)
+print(t)
+
+# dict
+d = {1:"a", 2:"b"}
+print(d)
